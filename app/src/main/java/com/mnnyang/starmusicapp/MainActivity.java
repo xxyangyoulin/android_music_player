22816676b5886a46b30dedf2494d55baee4bdc5b
mnnyang <mnnyang@outlook.com>
2018-01-13 00:24:59 +0800

    计划重构项目

---
+++ app/src/main/java/com/mnnyang/starmusicapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mnnyang.starmusicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //哈哈哈你好
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
