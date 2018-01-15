dccbe390a970ee166f6ddebdff753d85d2f9be06
mnnyang <mnnyang@outlook.com>
2018-01-16 01:17:19 +0800

    工具栏处理

--- app/src/main/java/com/mnnyang/starmusicapp/view/MainActivity.java
+++ app/src/main/java/com/mnnyang/starmusicapp/view/MainActivity.java
@@ -2,24 +2,31 @@ package com.mnnyang.starmusicapp.view;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.mnnyang.starmusicapp.BaseActivity;
 import com.mnnyang.starmusicapp.R;
-import com.mnnyang.starmusicapp.util.ScreenUtils;
 import com.mnnyang.starmusicapp.view.widget.NavItemView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.niv_setting)
     NavItemView nivSetting;
-
-    @Override
-    protected void beforeInit(Bundle state) {
-        ScreenUtils.setSystemBarTransparent(this);
-    }
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
 
     @Override
     protected int getContentLayout() {
@@ -29,6 +36,35 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView(Bundle state) {
         nivSetting.setSelected(true);
+        initToolbarDrawerLayout();
+    }
+
+
+    private void initToolbarDrawerLayout() {
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name,
+                R.string.app_name);
+        toggle.syncState();
+        drawerLayout.setDrawerListener(toggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                break;
+        }
+        return true;
     }
 
     @Override
@@ -36,6 +72,7 @@ public class MainActivity extends BaseActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    /*TODO 使用注解一次性*/
     @OnClick(R.id.niv_setting)
     public void setting() {
         Toast.makeText(this, "fff", Toast.LENGTH_SHORT).show();
